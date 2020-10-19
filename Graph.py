cd151e616827506efa908fc63bf945ea9749e008
sumamovva <movvasuma101@gmail.com>
2020-10-18 23:20:55 -0400

    compiles test if works

--- Graph.py
+++ Graph.py
@@ -1,12 +1,4 @@
 # Do not change the name of this file or the class name.
-
-# adjacency list representation 
-class AdjNode:
-    
-    def __init__(self, value):
-        self.vertex = value
-        self.next = None
-
 class Graph:
 
     def __init__(self, file_path):
@@ -41,26 +33,11 @@ class Graph:
                 egdes = [first_vertex]
                 graph[second_vertex].append(first_vertex)
 
-                """node = AdjNode(second_vertex)
-                node.next = graph[first_vertex]
-                graph[first_vertex] = node
-
-                node = AdjNode(first_vertex)
-                node.next = graph[second_vertex]
-                graph[second_vertex] = node"""
-
         return graph
     
     def print_graph(self):
-        """for i in range(self.n_nodes):
-            print("v " + str(i) + ": ", end="")
-            temp = self.graph_repr[i]
-            while temp:
-                print(" -> {}".format(temp.vertex), end="")
-                temp = temp.next
-            print(" \n")"""
         for i in range(self.n_nodes):
-            print(i, self.graph_repr[i])
+                print(i, self.graph_repr[i])
 
     def check_2colorable(self):
         """Determine whether or not the graph is 2-colorable and return colors.
@@ -73,14 +50,67 @@ class Graph:
             nodes of the same color don't share an edge. If graph is not
             2-colorable, return [].
         """
-        pass
+        # initializes coloring adjacent list with all -1s (uncolored)
+        coloring = [-1]*self.n_nodes
+
+        """coloring[0] = 0 # init src to color 0
+
+        queue = []
+        queue.append(1)
+        
+        while queue:
+            start = queue.pop()
+
+            for dest in self.graph_repr[start]:
+                if start == dest: # self loop
+                    print("[]")
+                    return []
+
+            for dest in self.graph_repr[start]:
+                # edge exists from start to dest and dest is -1 (uncolored)
+                if coloring[dest] == -1:
+                    if coloring[start] == 0: # set dest to opposite color as start
+                        coloring[dest] = 1 
+                    else:
+                        coloring[dest] = 0
+                    queue.append(dest)
+
+                # edge exists from start to dest and dest is colored with same color as start
+                elif coloring[dest] == coloring[start]:
+                    return []
+
+        print(coloring)
+        return coloring
+        """
+        for i in range(self.n_nodes):
+                if coloring[i] == -1: # if start is uncolored
+                    coloring[i] = 0 # init to BLACK
+                    queue = []
+                    queue.append(i)
+                    while queue:
+                        start = queue.pop()
+                        for connected in self.graph_repr[start]:
+                                if coloring[connected] != -1:
+                                    if coloring[connected] == coloring[start]:
+                                        print("[]")
+                                        return []
+                                else:
+                                    if coloring[start] == 0:
+                                        coloring[connected] = 1
+                                    else:
+                                        coloring[connected] = 0
+                                    queue.append(connected)
+        print(coloring)
+        return coloring        
+
+
 
 def main():
     #Main function, you can edit as needed
     #Extra import may also be made locally in this function
 
    graph = Graph('data/largegraph1')
-   graph.print_graph()
+   # graph.print_graph()
    colors = graph.check_2colorable()
 
 
